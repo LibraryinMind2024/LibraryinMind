0d7ba37d9b955354d953df84b1d439fced456855
YugyeongChoi <xyukyeong@naver.com>
2024-09-25 00:13:12 +0900

    404 에러 해결

--- src/main/java/sejong/libraryinmind/controller/BookAPIController.java
+++ src/main/java/sejong/libraryinmind/controller/BookAPIController.java
@@ -2,13 +2,15 @@ package sejong.libraryinmind.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BookAPIController {
 
     @GetMapping("/list")
     public String list(){
-        return "/list";
+        System.out.println("hiiiiii");
+        return "list";
     }
 
 }
