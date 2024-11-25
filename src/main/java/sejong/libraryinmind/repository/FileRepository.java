ea7765d53475c7f8e5506c58720bb3df93057abd
YugyeongChoi <xyukyeong@naver.com>
2024-11-25 13:28:43 +0900

    Fix untracked files venv

--- src/main/java/sejong/libraryinmind/repository/FileRepository.java
+++
@@ -1,7 +0,0 @@
-package sejong.libraryinmind.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sejong.libraryinmind.entity.FileEntity;
-
-public interface FileRepository extends JpaRepository<FileEntity, Long> {
-}
